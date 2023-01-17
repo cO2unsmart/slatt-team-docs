60782273d6aee15049ced5f071a480e3f4684fef
Super Wings <109900136+cO2unsmart@users.noreply.github.com>
2023-01-17 19:25:33 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -11,7 +11,7 @@ const config: DocsThemeConfig = {
   },
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© %s 2023 CO2.' % getFullYear(),
   },
 }
 
